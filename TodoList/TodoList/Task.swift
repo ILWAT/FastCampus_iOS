d054b27825c8f21e7540ec1bb9420015409801d7
J.H.Moon <moonjh587@naver.com>
2022-11-07 23:43:55 +0900

    11.7 commit

---
+++ TodoList/TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 문정호 on 2022/11/07.
+//
+
+import Foundation
+
+struct Task{
+    var title: String
+    var done: Bool
+}
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var tasks = [Task]()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: nil, preferredStyle: .alert)
+        //alert은 가운데 띄워지는 안내창 ActionSheet는 하단에 메뉴바 같이 생긴 안내창
+        let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+            alert.textFields?[0].text
+        })
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+        alert.addTextField(configurationHandler: {textField in
+            textField.placeholder = "할 일을 입력해주세요"
+        })
+        self.present(alert,animated: true,completion: nil)
+    }
+    
 
 }
 
